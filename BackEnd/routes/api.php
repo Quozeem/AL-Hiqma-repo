d6fcff82b0f09474f4c21b052a663d30461fd1e3
Ademola Quozeem <72305068+Quozeem@users.noreply.github.com>
2024-04-30 07:44:10 -0700

    Update api.php

--- BackEnd/routes/api.php
+++ BackEnd/routes/api.php
@@ -11,8 +11,11 @@ Route::get('/user', function (Request $request) {
 
 
 Route::get('/external-books', [ExternalBookController::class, 'getExternalBooks']);
-Route::post('/v1/books', [ExternalBookController::class, 'create']);
-Route::get('/v1/books', [ExternalBookController::class, 'index']);
-Route::patch('/v1/books', [ExternalBookController::class, 'updates']);
-Route::delete('/v1/books/{id}', [ExternalBookController::class, 'destroy']);
-Route::get('/v1/books/{id}', [ExternalBookController::class, 'show']);
+
+Route::prefix('v1')->group(function () {
+Route::post('/books', [ExternalBookController::class, 'create']);
+Route::get('/books', [ExternalBookController::class, 'index']);
+Route::patch('/books', [ExternalBookController::class, 'updates']);
+Route::delete('/books/{id}', [ExternalBookController::class, 'destroy']);
+Route::get('/books/{id}', [ExternalBookController::class, 'show']);
+});
