b27ffcc02e8da1f875b52d2829bdacfac74cd6d6
Quozeem <qmanrocket@gmail.com>
2024-05-12 14:16:23 +0100

    update

--- FrontEnd/src/component/Redux/Index.jsx
+++ FrontEnd/src/component/Redux/Index.jsx
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   book: [],
   Category: [],
-  dataNotAccount: "",
+  testdataNotAccount: "",
   message: "",
   Editbook: "",
 };
