2a1ef7316116c8be1aa5737cc0f0a62b3d025b8c
Ademola Quozeem <72305068+Quozeem@users.noreply.github.com>
2024-05-12 14:25:53 +0100

    Update Index.jsx

--- FrontEnd/src/component/Redux/Index.jsx
+++ FrontEnd/src/component/Redux/Index.jsx
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   book: [],
   testCategory: [],
-  dataNotAccount: "",
+  testdataNotAccount: "",
   message: "",
   testEditbook: "",
 };
