7d503d453624891cbb0932ee2490a0e65f5a38a4
Ademola Quozeem <72305068+Quozeem@users.noreply.github.com>
2024-10-20 21:28:15 +0100

    Update web.php

--- BackEnd/routes/web.php
+++ BackEnd/routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Hakim\StudentEnrollController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/csrf-token', function () {
-    return response()->json(['csrf_token' => csrf_token()]);
-});
+    return response()->json(['csrf_token' => csrf_token()]
+                            
+                            ###############AL GIQM ROUTE ########
+
+ Route::any('/hakim', [StudentEnrollController::class, 'filterEnrollments'])->name('filterEnrollments');
+Route::get('/faculty', [StudentEnrollController::class, 'faculty'])->name('faculty');
+Route::get('/faculty/{faculty:id}', [StudentEnrollController::class, 'program'])->name('program');
